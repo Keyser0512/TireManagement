1d6ee457d818e5ec3faf2f1ff3bd2c280b4b5e2d
jurek-nky <jurek.nit@gmail.com>
2021-11-09 17:22:03 +0100

    initialCommit, spring demo application

---
+++ src/main/java/com/test/TireManagementApiApplication.java
@@ -0,0 +1,14 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TireManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TireManagementApiApplication.class, args);
+
+	}
+
+}
